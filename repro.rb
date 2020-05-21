3617b75385ad022f0405d2e12ec500af61659fb0
Quinn Daley <quinn@fishpercolator.co.uk>
2020-05-21 17:13:33 +0100

    Curiouser and curiouser

--- repro.rb
+++ repro.rb
@@ -22,23 +22,39 @@ def io_copy_stream_test(name)
   puts "#{name}, IO.copy_stream: #{File.new(FN).size}"
 end
 
-test 'File.open, local' do
+test 'open, local' do
   File.open('./example.png')
 end
-test 'File.open, mounted' do
+test 'open, mounted' do
   File.open('/mnt/example.png')
 end
 
-test 'Tempfile.new + copy_file preserve=false, mounted' do
+test 'tempfile + copy_file, local' do
+  tf = Tempfile.new(['example', '.png'])
+  tf.set_encoding(Encoding::BINARY)
+  FileUtils.copy_file('./example.png', tf.path)
+  tf
+end
+
+test 'tempfile + copy_file, mounted' do
   tf = Tempfile.new(['example', '.png'])
   tf.set_encoding(Encoding::BINARY)
   FileUtils.copy_file('/mnt/example.png', tf.path)
   tf
 end
 
-test 'Tempfile.new + copy_file preserve=true, mounted' do
+test 'tempfile + copy_file + noop utime, mounted' do
   tf = Tempfile.new(['example', '.png'])
   tf.set_encoding(Encoding::BINARY)
-  FileUtils.copy_file('/mnt/example.png', tf.path, true)
+  FileUtils.copy_file('/mnt/example.png', tf.path)
+  File.utime tf.atime, tf.mtime, tf.path
+  tf
+end
+
+test 'tempfile + copy_file + noop chmod, mounted' do
+  tf = Tempfile.new(['example', '.png'])
+  tf.set_encoding(Encoding::BINARY)
+  FileUtils.copy_file('/mnt/example.png', tf.path)
+  tf.chmod tf.lstat.mode
   tf
 end
